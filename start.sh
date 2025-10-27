f82492b09da11a631a4d0766bde9c0216dae0503
hackerhohacker <vampirehemant45@gmail.com>
2025-10-26 20:01:44 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Start Lavalink in the background
-java -jar Lavalink.jar > lavalink.log 2>&1 &
+java -jar Lavalink.jar &
 
 # Wait 2 minutes for Lavalink to fully start and for the Discord client to connect.
-# This prevents Render from killing the process before the bot is fully initialized.
 echo "Lavalink started in background. Sleeping for 120 seconds to allow full startup..."
 sleep 120
 
