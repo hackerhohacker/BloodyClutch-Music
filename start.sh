b154a695a967dc4cb9cbc2387d4df48662b20002
hackerhohacker <vampirehemant45@gmail.com>
2025-10-26 19:10:09 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Start Lavalink in the background
 java -jar Lavalink.jar &
 
-# INCREASED DELAY: Wait 60 seconds to guarantee Lavalink has fully initialized (it takes ~41 seconds).
+# CRITICAL FIX: Wait 60 seconds to guarantee Lavalink has fully initialized (it takes ~41 seconds).
 sleep 60
 
 # Start the Discord Bot
