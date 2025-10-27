76e8dc3a935369421ed40435149c35e0dc2bc760
hackerhohacker <vampirehemant45@gmail.com>
2025-10-26 20:42:30 -0800

    Update musicUtils.js

--- src/utils/musicUtils.js
+++ src/utils/musicUtils.js
@@ -1,7 +1,7 @@
 const nodes = [
     {
         name: 'render-internal-node',
-        host: '127.0.0.1', // Internal loopback address
+        host: '127.0.0.1', // Always use loopback address for internal container communication
         port: 443,        // Must match application.yml
         auth: 'bloodyclutch123', // Must match application.yml
     },
